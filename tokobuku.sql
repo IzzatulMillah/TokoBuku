c0a63c662d864a8d9c0d7eb46c6b30edf913c5ca
IzzatulMillah <izzatulmillah27@gmail.com>
2018-11-21 16:29:52 +0700

    progress masih di nota pembelian

--- tokobuku.sql
+++ tokobuku.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: Nov 19, 2018 at 04:58 PM
+-- Generation Time: Nov 21, 2018 at 04:29 PM
 -- Server version: 5.7.23-0ubuntu0.16.04.1-log
 -- PHP Version: 7.0.32-0ubuntu0.16.04.1
 
@@ -29,8 +29,9 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `buku` (
-  `id` bigint(50) NOT NULL,
+  `id` bigint(20) NOT NULL,
   `judul` varchar(100) NOT NULL,
+  `tipe_buku` varchar(100) NOT NULL,
   `saldo` int(10) NOT NULL,
   `harga` double NOT NULL,
   `flag_aktif` varchar(10) NOT NULL
@@ -40,8 +41,36 @@ CREATE TABLE `buku` (
 -- Dumping data for table `buku`
 --
 
-INSERT INTO `buku` (`id`, `judul`, `saldo`, `harga`, `flag_aktif`) VALUES
-(1542620361262, 'The Talking Cat Kitty', 23, 123456, 'Y');
+INSERT INTO `buku` (`id`, `judul`, `tipe_buku`, `saldo`, `harga`, `flag_aktif`) VALUES
+(1542620361262, 'The Talking Cat Kitty', '', 23, 123456, 'Y'),
+(1542621857470, 'The Harry Potter and The Goblet of Fire', '', 120, 4500000, 'Ya');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `detail_nota`
+--
+
+CREATE TABLE `detail_nota` (
+  `id_nota` int(10) NOT NULL,
+  `judul` varchar(200) NOT NULL,
+  `jumlah_buku` int(10) NOT NULL,
+  `harga_satuan` double NOT NULL,
+  `harga_total` double NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `header_nota`
+--
+
+CREATE TABLE `header_nota` (
+  `id` int(10) NOT NULL,
+  `nama` varchar(100) NOT NULL,
+  `alamat` varchar(200) NOT NULL,
+  `tanggal` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -55,6 +84,48 @@ CREATE TABLE `pembeli` (
   `alamat` varchar(100) NOT NULL,
   `kota` varchar(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `pembeli`
+--
+
+INSERT INTO `pembeli` (`id`, `nama`, `alamat`, `kota`) VALUES
+(3, 'Izzatul Millah', 'kejawan', 'null'),
+(4, 'Rizaldi', 'Panjang Jiwo Permai', 'null'),
+(5, 'Jenny', 'Menanggal Utara', 'null'),
+(6, 'Tasya', 'Kemang', 'null');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `detail_nota`
+--
+ALTER TABLE `detail_nota`
+  ADD PRIMARY KEY (`id_nota`);
+
+--
+-- Indexes for table `header_nota`
+--
+ALTER TABLE `header_nota`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `pembeli`
+--
+ALTER TABLE `pembeli`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `pembeli`
+--
+ALTER TABLE `pembeli`
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
